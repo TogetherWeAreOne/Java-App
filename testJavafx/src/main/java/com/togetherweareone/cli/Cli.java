a4c5302d01c4ddb577c11dd5e3e9265e0ccd8bf2
Ad <adrienroux94@gmail.com>
2021-06-24 00:46:48 +0200

    Update Cli.java

--- testJavafx/src/main/java/com/togetherweareone/cli/Cli.java
+++ testJavafx/src/main/java/com/togetherweareone/cli/Cli.java
@@ -2,6 +2,8 @@ package com.togetherweareone.cli;
 
 import com.togetherweareone.utilities.ConsoleColors;
 
+import java.util.Scanner;
+
 public class Cli {
     public Cli(){
         start();
@@ -10,36 +12,52 @@ public class Cli {
     private void start() {
 
         printHugeTitle();
+        printPresentation();
+        printLogin();
 
-        printInformation("Bienvenue sur Together We Are One ! ( CLI version )\n" +
-                "Vous pouvez utilisez les différentes commandes pour naviguer dans l'arborescence !\n" +
-                "N'oubliez que vous pouvez toujours éxécuter ce programme en version graphique ou bien aller voir l'interface web sur notre site : *insérer le lien*");
-
-        print("Voici une liste de blablabla");
     }
 
+    /*
+    Fonctions d'écriture de la console communes
+     */
     private void printHugeTitle(){
         paragraph();
-        print("-------------------------------------------------------------------------------------------------");
-        print(" _____                 _   _                                                                  \n" +
+        printError("-------------------------------------------------------------------------------------------------");
+        printError(" _____                 _   _                                                                  \n" +
                 "/__   \\___   __ _  ___| |_| |__   ___ _ __  __      _____    __ _ _ __ ___    ___  _ __   ___ \n" +
                 "  / /\\/ _ \\ / _` |/ _ \\ __| '_ \\ / _ \\ '__| \\ \\ /\\ / / _ \\  / _` | '__/ _ \\  / _ \\| '_ \\ / _ \\\n" +
                 " / / | (_) | (_| |  __/ |_| | | |  __/ |     \\ V  V /  __/ | (_| | | |  __/ | (_) | | | |  __/\n" +
                 " \\/   \\___/ \\__, |\\___|\\__|_| |_|\\___|_|      \\_/\\_/ \\___|  \\__,_|_|  \\___|  \\___/|_| |_|\\___|\n" +
                 "            |___/                                                                             ");
-        print("-------------------------------------------------------------------------------------------------");
+        printError("-------------------------------------------------------------------------------------------------");
         paragraph();
     }
 
+    private void printPresentation(){
+        printInformation("Bienvenue sur Together We Are One ! ( CLI version ) © 2021 TWAO.  All rights reserved.\n" +
+                "Vous pouvez utilisez les différentes commandes pour naviguer dans l'arborescence !\n" +
+                "N'oubliez que vous pouvez toujours exécuter ce programme en version graphique ou bien aller voir l'interface web sur notre site : *insérer le lien*");
+    }
+
+    private void printLogin(){
+        String entry = ask("Veuillez entrer vos informations de connexion :");
+        printHighlight("Vous avez entré:", entry);
+    }
+
+
+    /*
+    Fonctions usuelles d'écriture de la console
+     */
     private void print(String text){
         System.out.println(ConsoleColors.WHITE + text + ConsoleColors.RESET);
-        paragraph();
+        line();
     }
 
     private void printTitleUnderlined(String text){
         System.out.println(ConsoleColors.CYAN_BOLD_UNDERLINED + text + ConsoleColors.RESET);
         paragraph();
     }
+
 
     private void printTitle(String text){
         System.out.println(ConsoleColors.CYAN_BRIGHT + text + ConsoleColors.RESET);
@@ -48,7 +66,7 @@ public class Cli {
 
     private void printAlert(String text){
         System.out.println(ConsoleColors.YELLOW_BRIGHT + text + ConsoleColors.RESET);
-        paragraph();
+        line();
     }
 
     private void printInformation(String text){
@@ -58,11 +76,29 @@ public class Cli {
 
     private void printError(String text){
         System.out.println(ConsoleColors.RED_BRIGHT + text + ConsoleColors.RESET);
-        paragraph();
+        line();
+    }
+
+    private void printHighlight(String text, String highlight){
+        System.out.print(ConsoleColors.GREEN_ITALIC + text + ConsoleColors.RESET + "  ");
+        System.out.println(ConsoleColors.YELLOW_BRIGHT + highlight + ConsoleColors.RESET);
+        line();
+    }
+
+    private String ask(String text){
+        System.out.print(ConsoleColors.GREEN_ITALIC + text + ConsoleColors.RESET + "  ");
+        Scanner sc = new Scanner(System.in);
+        String entry = sc.nextLine();
+        line();
+        return entry;
     }
 
     private void paragraph(){
         System.out.println();
+        System.out.println();
+    }
+
+    private void line(){
         System.out.println();
     }
 }
