dd1814c57a4f9c4022c590402b32e9ac91847d3b
Ad <adrienroux94@gmail.com>
2021-06-30 02:09:16 +0200

    BAT Script added path to executable so it can be run everywhere

--- launch.bat
+++ launch.bat
@@ -1,1 +1,3 @@
-java --module-path javafx\lib --add-modules javafx.controls,javafx.fxml -jar out\artifacts\testJavafx_jar\testJavafx.jar
+@ECHO OFF
+SET mypath=%~dp0
+java --module-path %mypath%javafx\lib --add-modules javafx.controls,javafx.fxml -jar %mypath%out\artifacts\testJavafx_jar\testJavafx.jar
