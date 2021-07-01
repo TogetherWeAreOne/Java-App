0d319b245f897d6dfa68687425bb9773d359ca5f
Ad <adrienroux94@gmail.com>
2021-07-01 06:48:52 +0200

    Update Main.java

--- testJavafx/src/main/java/com/togetherweareone/Main.java
+++ testJavafx/src/main/java/com/togetherweareone/Main.java
@@ -1,54 +1,27 @@
 package com.togetherweareone;
 
 import com.togetherweareone.cli.Cli;
+import com.togetherweareone.fx.Fx;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
-    boolean error = false;
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
-
-        /*Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
-        primaryStage.setTitle("TogetherWeAreOne Organisation");
-        primaryStage.setScene(new Scene(root, 1200, 800));
-
-        primaryStage.show();
-*/
-        /*ApiClient client = new ApiClient();
-        AuthService service = new AuthService();
-        ProjectService projectService = new ProjectService();
-        LoginRequest request = new LoginRequest("test@gmail.com","password");
-        CreateProjectRequest createProjectRequest = new CreateProjectRequest("testProjectInline", "testProjectInline");
-        //MultiValueMap<String, String> cookies = new LinkedMultiValueMap<String, String>();
-
-        Mono<User> user = service.login(client.getWebClient(), request);
-
-        user.doOnSuccess(System.out::println)
-                .doOnError(e ->  {
-                    error = true;
-                })
-                .onErrorReturn(new User())
-                .block();
-
-        if (error)
-            System.out.println("NO CREATION USER");
-        else {
-            Mono<Project> project = projectService.createProject(client.getWebClient(), createProjectRequest);
-            project.doOnSuccess(System.out::println)
-                    .onErrorReturn(new Project())
-                    .block();
-        }*/
-
-
+    public void start(Stage primaryStage) throws IOException {
+        new Fx(primaryStage);
     }
 
     public static void main(String[] args) {
-
-        Runtime.getRuntime().addShutdownHook(new Thread(Cli::endApp));
 
         if (args.length > 0 && args[0].equals("-cli")) {
             new Cli();
