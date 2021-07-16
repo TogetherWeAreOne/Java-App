b3d688b065d3afae310f43f55789fd133190c470
Ad <adrienroux94@gmail.com>
2021-07-16 11:22:20 +0200

    Update module-info.java

--- testJavafx/src/main/java/module-info.java
+++ testJavafx/src/main/java/module-info.java
@@ -25,8 +25,7 @@ module com.togetherweareone {
     requires org.reactivestreams;
     requires com.fasterxml.jackson.core;
     //requires org.controlsfx.controls;
-
-
+    
     opens com.togetherweareone.request.authRequest;
     opens com.togetherweareone.request.projectRequest;
     opens com.togetherweareone.request.checklistRequest;
@@ -34,7 +33,6 @@ module com.togetherweareone {
     opens com.togetherweareone.request.optionRequest;
     opens com.togetherweareone.request.stickerRequest;
     opens com.togetherweareone.request.taskRequest;
-
     opens com.togetherweareone to javafx.fxml;
     opens com.togetherweareone.fx.controllers to javafx.fxml;
 
@@ -42,6 +40,6 @@ module com.togetherweareone {
     exports com.togetherweareone.controller;
     exports com.togetherweareone.models;
     exports com.togetherweareone.fx to javafx.fxml, javafx.graphics;
-
+    exports com.togetherweareone.api;
 
 }
